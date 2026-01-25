68fef027fa7b779f14ab8820d833dd3a1afa679e
Dmitriy Zaharov <asparabus@ya.ru>
2026-01-25 21:30:23 +0300

    Занятие 2. Атрибуты и методы. Лабораторные задания (класс Library) - выполнено.

--- Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
+++ Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
@@ -14,17 +14,51 @@ BOOKS_DATABASE = [
 
 
 # TODO Импортируйте и скопируйте ранее написанный класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        """Конструктор класса Book.
 
+        Args:
+            id_ (int): Идентификатор книги
+            name (str): Название книги
+            pages (int): Количество страниц в книге
+        """
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление книги в формате 'Книга "название_книги"'.
+
+        Returns:
+            str: Строковое представление книги
+        """
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        """Возвращает строку, по которой можно инициализировать точно такой же экземпляр.
+
+        Returns:
+            str: Строка для инициализации экземпляра
+        """
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
 class Library:
 
-    def __init__(self, books):
+    def __init__(self, books=None):
         """
         Не забудьте про 'Конструктор должен принимать необязательный аргумент со значением по умолчанию. Если пользователь
         его не передал, то библиотека инициализируется с пустым списком книг.'
         :param books:
         """
-        pass # TODO дописать метод
+        """
+         Конструктор класса Library.
+
+         Args:
+             books (list, optional): Список книг для инициализации библиотеки. 
+                                    Если не передан, используется пустой список.
+         """
+        self.books = books if books is not None else []
 
     def get_next_book_id(self):
         """
@@ -33,6 +67,17 @@ class Library:
         :return:
         """
         pass # TODO дописать метод
+        """   
+        Возвращает идентификатор для добавления новой книги.
+
+        Returns:
+            int: 1 если библиотека пуста, иначе id последней книги + 1
+        """
+        if not self.books:
+            return 1
+        # Предполагаем, что у каждой книги есть атрибут 'id'
+        last_book_id = self.books[-1].get('id') if isinstance(self.books[-1], dict) else self.books[-1].id
+        return last_book_id + 1
 
     def get_index_by_book_id(self, id_):
         """
@@ -43,7 +88,21 @@ class Library:
         :return: индекс, где лежит книга в списке книг
         """
         pass # TODO дописать метод
+        """
+        Возвращает индекс книги в списке по её идентификатору.
+        Args:
+            book_id (int): Идентификатор искомой книги
+        Returns:
+            int: Индекс книги в списке
+        Raises:
+            ValueError: Если книги с указанным id не существует
+        """
+        for index, book in enumerate(self.books):
+            current_id = book.get('id') if isinstance(book, dict) else book.id
+            if current_id == id_:
+                return index
 
+        raise ValueError("Книги с запрашиваемым id не существует")
 
 if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
@@ -56,3 +115,9 @@ if __name__ == '__main__':
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+
+    # Пример 3: Попытка найти несуществующую книгу
+    # try:
+    #     library_with_books.get_index_by_book_id(10)
+    # except ValueError as e:
+    #     print(e)  # "Книги с запрашиваемым id не существует"
